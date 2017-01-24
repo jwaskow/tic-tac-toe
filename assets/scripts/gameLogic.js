ecd2bec9f555248e5df8f3471930d0ba60addfeb
jwaskow <thewasko@gmail.com>
2017-01-24 16:46:05 -0500

    Update checkWinner function
    checkWinner function was incomplete

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -13,15 +13,15 @@ let board = ['', '', '', '', '', '', '', '', ''];
 // [2,4,6]
 
 const checkWinner = function (board) {
-  if ((board[0] === 'x' && board[1] === 'x' && board[2]) || (board[3] === 'x' && board[4] === 'x' && board[5]) ||
-      (board[6] === 'x' && board[7] === 'x' && board[8]) || (board[0] === 'x' && board[3] === 'x' && board[6]) ||
-      (board[1] === 'x' && board[4] === 'x' && board[7]) || (board[2] === 'x' && board[5] === 'x' && board[8]) ||
-      (board[0] === 'x' && board[4] === 'x' && board[8]) || (board[2] === 'x' && board[4] === 'x' && board[6])) {
+  if ((board[0] === 'x' && board[1] === 'x' && board[2] === 'x') || (board[3] === 'x' && board[4] === 'x' && board[5] === 'x') ||
+      (board[6] === 'x' && board[7] === 'x' && board[8] === 'x') || (board[0] === 'x' && board[3] === 'x' && board[6] === 'x') ||
+      (board[1] === 'x' && board[4] === 'x' && board[7] === 'x') || (board[2] === 'x' && board[5] === 'x' && board[8] === 'x') ||
+      (board[0] === 'x' && board[4] === 'x' && board[8] === 'x') || (board[2] === 'x' && board[4] === 'x' && board[6] === 'x')) {
         console.log('x is the winner!');
-} else if ((board[0] === 'x' && board[1] === 'x' && board[2]) || (board[3] === 'x' && board[4] === 'x' && board[5]) ||
-      (board[6] === 'o' && board[7] === 'o' && board[8]) || (board[0] === 'o' && board[3] === 'o' && board[6]) ||
-      (board[1] === 'o' && board[4] === 'o' && board[7]) || (board[2] === 'o' && board[5] === 'o' && board[8]) ||
-      (board[0] === 'o' && board[4] === 'o' && board[8]) || (board[2] === 'o' && board[4] === 'o' && board[6])) {
+} else if ((board[0] === 'x' && board[1] === 'x' && board[2] === 'o') || (board[3] === 'x' && board[4] === 'x' && board[5] === 'o') ||
+      (board[6] === 'o' && board[7] === 'o' && board[8] === 'o') || (board[0] === 'o' && board[3] === 'o' && board[6] === 'o') ||
+      (board[1] === 'o' && board[4] === 'o' && board[7] === 'o') || (board[2] === 'o' && board[5] === 'o' && board[8] === 'o') ||
+      (board[0] === 'o' && board[4] === 'o' && board[8] === 'o') || (board[2] === 'o' && board[4] === 'o' && board[6] === 'o')) {
         console.log('o is the winner!');
       }
     };
