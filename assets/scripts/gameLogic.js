f4091e2ab787a003929965d7304eed264aeecefd
jwaskow <thewasko@gmail.com>
2017-01-25 10:39:52 -0500

    Change game logic to a simpler version

--- assets/scripts/gameLogic.js
+++ assets/scripts/gameLogic.js
@@ -1,37 +1,70 @@
 'use strict';
 
-const store = require('./store');
 
-let board = ['', '', '', '', '', '', '', '', ''];
 
-// Winning Combos:
-// [0,1,2]
-// [3,4,5]
-// [6,7,8]
-// [0,3,6]
-// [1,4,7]
-// [2,5,8]
-// [0,4,8]
-// [2,4,6]
+let board = ['', '', '', '', '', '', '', '', ''];  // board is an empty grid
+
+let turnCount = 0;
+
+// checkWinner checks if either player has a winning combination.
+// if any of the winning combos are 'true', a winner is declared.
 
 const checkWinner = function (board) {
-  if ((board[0] === 'x' && board[1] === 'x' && board[2] === 'x') || (board[3] === 'x' && board[4] === 'x' && board[5] === 'x') ||
-      (board[6] === 'x' && board[7] === 'x' && board[8] === 'x') || (board[0] === 'x' && board[3] === 'x' && board[6] === 'x') ||
-      (board[1] === 'x' && board[4] === 'x' && board[7] === 'x') || (board[2] === 'x' && board[5] === 'x' && board[8] === 'x') ||
-      (board[0] === 'x' && board[4] === 'x' && board[8] === 'x') || (board[2] === 'x' && board[4] === 'x' && board[6] === 'x')) {
+  if (((board[0] === 'x') && (board[1] === 'x') && (board[2] === 'x')) || ((board[3] === 'x') && (board[4] === 'x') && (board[5] === 'x')) ||
+      ((board[6] === 'x') && (board[7] === 'x') && (board[8] === 'x')) || ((board[0] === 'x') && (board[3] === 'x') && (board[6] === 'x')) ||
+      ((board[1] === 'x') && (board[4] === 'x') && (board[7] === 'x')) || ((board[2] === 'x') && (board[5] === 'x') && (board[8] === 'x')) ||
+      ((board[0] === 'x') && (board[4] === 'x') && (board[8] === 'x')) || ((board[2] === 'x') && (board[4] === 'x') && (board[6] === 'x'))) {
         console.log('x is the winner!');
-} else if ((board[0] === 'x' && board[1] === 'x' && board[2] === 'o') || (board[3] === 'x' && board[4] === 'x' && board[5] === 'o') ||
+        return 'x is the winner!';
+} else if ((board[0] === 'o' && board[1] === 'o' && board[2] === 'o') || (board[3] === 'o' && board[4] === 'o' && board[5] === 'o') ||
       (board[6] === 'o' && board[7] === 'o' && board[8] === 'o') || (board[0] === 'o' && board[3] === 'o' && board[6] === 'o') ||
       (board[1] === 'o' && board[4] === 'o' && board[7] === 'o') || (board[2] === 'o' && board[5] === 'o' && board[8] === 'o') ||
       (board[0] === 'o' && board[4] === 'o' && board[8] === 'o') || (board[2] === 'o' && board[4] === 'o' && board[6] === 'o')) {
         console.log('o is the winner!');
+        return 'o is the winner!';
+      }
+      else if (turnCount > 8) {
+        console.log('This game is a draw!');
+        return 'This game is a draw!';
       }
     };
 
+const changeTurn = function (num) {
+    if (board[num] !== '') {
+      console.log("Please pick an empty square!");
+      return "Please pick an empty square!";
+    }
 
+    if (turnCount % 2 === 0) {
+       board[num] = "o";
+    } else {
+       board[num] = "x";
+   }
+     turnCount++;
+     console.log(turnCount);
+     return turnCount;
+};
+
+const testGame = function (num) {
+  changeTurn(num);
+  console.log(board);
+  return checkWinner(board);
+};
+
+// resetBoard iterated through the board array and assigns each item an empty string
 
 const resetBoard = function () {
   for (let i = 0; i < board.length; i++) {
     board[i] = '';
   }
+  turnCount = 0;
+  console.log(board);
+};
+
+
+module.exports = {
+  board,
+  changeTurn,
+  checkWinner,
+  resetBoard,
 };
--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const store = {
-  turn: "x"
 };
 
 module.exports = store;
